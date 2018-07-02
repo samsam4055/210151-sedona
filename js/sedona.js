a5487538a858f19c18c29a4eb94bf3058f32db50
samsam4055 <39153320+samsam4055@users.noreply.github.com>
2018-07-02 15:15:20 +0300

    ПП

--- js/sedona.js
+++ js/sedona.js
@@ -1,4 +1,3 @@
-
 var mainPage = "index.html";
 
 var hotelsPage = "hotels.html"
@@ -7,140 +6,140 @@ var emptyPage = "empty.html"
 var path = window.location.pathname;
 var nameCurPage = path.split("/").pop();
 
-if ( nameCurPage == mainPage) {   // если текущая страница основная - выполняется код ниже
+if (nameCurPage == mainPage) { // если текущая страница основная - выполняется код ниже
 
-// попап
-var form = document.querySelector(".search-form");
-var searchQuantityAdults = document.querySelector("#search-quantity-adults");
-var searchQuantityChildren = document.querySelector("#search-quantity-children");
-var popupWindow = document.querySelector(".popup-window");
+  // попап
+  var form = document.querySelector(".search-form");
+  var searchQuantityAdults = document.querySelector("#search-quantity-adults");
+  var searchQuantityChildren = document.querySelector("#search-quantity-children");
+  var popupWindow = document.querySelector(".popup-window");
 
-// календарь
+  // календарь
 
-    $(function() {
+  $(function() {
 
-      $.datepicker.setDefaults($.datepicker.regional['ru']);
-      $('#search-date-arrival').datepicker({
-        showOn: "focus",
-		    minDate: "0",
-        dateFormat: "dd MM yy",
-       });
-
-      $('#search-date-departure').datepicker({
-        showOn: "focus",
-		    minDate: "1",
-        dateFormat: "dd MM yy",
-		});
+    $.datepicker.setDefaults($.datepicker.regional['ru']);
+    $('#search-date-arrival').datepicker({
+      showOn: "focus",
+      minDate: "0",
+      dateFormat: "dd MM yy",
     });
 
-    $('#call-calendar-arrival').click(function() {
-      $('#search-date-arrival').focus();
+    $('#search-date-departure').datepicker({
+      showOn: "focus",
+      minDate: "1",
+      dateFormat: "dd MM yy",
     });
+  });
 
-    $('#call-calendar-departure').click(function() {
-      $('#search-date-departure').focus();
-    });
+  $('#call-calendar-arrival').click(function() {
+    $('#search-date-arrival').focus();
+  });
 
-// попап
+  $('#call-calendar-departure').click(function() {
+    $('#search-date-departure').focus();
+  });
 
-    popupWindow.classList.remove("popup-on");
-      $('#call-popup').click(function() {
-        popupWindow.classList.toggle("popup-on");
-        popupWindow.classList.remove("modal-error");
-      });
+  // попап
 
-      window.addEventListener("keydown", function (evt) {
-        if (evt.keyCode === 27) {
-          evt.preventDefault();
-        popupWindow.classList.remove("modal-error");
-        popupWindow.classList.remove("popup-on");
-          }
-        });
+  popupWindow.classList.remove("popup-on");
+  $('#call-popup').click(function() {
+    popupWindow.classList.toggle("popup-on");
+    popupWindow.classList.remove("modal-error");
+  });
 
-    form.addEventListener("submit", function (evt) {
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      popupWindow.classList.remove("modal-error");
+      popupWindow.classList.remove("popup-on");
+    }
+  });
 
-    if (searchQuantityAdults.value <= 0 || searchQuantityChildren.value < 0 ) {
-        evt.preventDefault();
-	    popupWindow.classList.remove("modal-error");
+  form.addEventListener("submit", function(evt) {
+
+    if (searchQuantityAdults.value <= 0 || searchQuantityChildren.value < 0) {
+      evt.preventDefault();
+      popupWindow.classList.remove("modal-error");
       popupWindow.offsetWidth = popupWindow.offsetWidth;
       popupWindow.classList.add("modal-error");
     }
-    });
-}    // закрывается иф певой страницы
+  });
+} // закрывается иф певой страницы
 
-if ( nameCurPage == hotelsPage) {  // если текущая страница отели - выполняется код ниже
+if (nameCurPage == hotelsPage) { // если текущая страница отели - выполняется код ниже
 
-    // отели
-    var link = document.querySelector(".up-arrow-js");
-    var sortUp = document.querySelector(".catalog-items");
+  // отели
+  var link = document.querySelector(".up-arrow-js");
+  var sortUp = document.querySelector(".catalog-items");
 
-    var downLink = document.querySelector(".down-arrow-js");
-    var sortDown = document.querySelector(".catalog-items");
+  var downLink = document.querySelector(".down-arrow-js");
+  var sortDown = document.querySelector(".catalog-items");
 
-    var arrowUp = document.querySelector(".up-arrow");
-    var arrowCur = document.querySelector(".current-arrow");
+  var arrowUp = document.querySelector(".up-arrow");
+  var arrowCur = document.querySelector(".current-arrow");
 
-    var arrowDown = document.querySelector(".down-arrow");
+  var arrowDown = document.querySelector(".down-arrow");
 
-    // отели сортировка
-    var byPrice = document.querySelector(".header-catalog-sorting li:nth-child(1)");
-    var byType = document.querySelector(".header-catalog-sorting li:nth-child(2)");
-    var byRating = document.querySelector(".header-catalog-sorting li:nth-child(3)");
+  // отели сортировка
+  var byPrice = document.querySelector(".header-catalog-sorting li:nth-child(1)");
+  var byType = document.querySelector(".header-catalog-sorting li:nth-child(2)");
+  var byRating = document.querySelector(".header-catalog-sorting li:nth-child(3)");
 
-    // страница отелей
+  // страница отелей
 
-      link.addEventListener("click", function(event) {
-        event.preventDefault();
-        sortUp.classList.add("catalog-items-js");
-        arrowUp.classList.add("current-arrow");
-        arrowDown.classList.remove("current-arrow");
-      });
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    sortUp.classList.add("catalog-items-js");
+    arrowUp.classList.add("current-arrow");
+    arrowDown.classList.remove("current-arrow");
+  });
 
-      downLink.addEventListener("click", function(event) {
-      event.preventDefault();
-      sortDown.classList.remove("catalog-items-js");
-      arrowUp.classList.remove("current-arrow");
-      arrowDown.classList.add("current-arrow");
-      });
+  downLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    sortDown.classList.remove("catalog-items-js");
+    arrowUp.classList.remove("current-arrow");
+    arrowDown.classList.add("current-arrow");
+  });
 
-    // активная сортировка шапки второй страницы
+  // активная сортировка шапки второй страницы
 
-      byPrice.addEventListener("click", function(event) {
-        event.preventDefault();
-        byPrice.classList.add("current-sorting");
-        byType.classList.remove("current-sorting");
-        byRating.classList.remove("current-sorting");
-      });
+  byPrice.addEventListener("click", function(event) {
+    event.preventDefault();
+    byPrice.classList.add("current-sorting");
+    byType.classList.remove("current-sorting");
+    byRating.classList.remove("current-sorting");
+  });
 
-      byType.addEventListener("click", function(event) {
-        event.preventDefault();
-        byPrice.classList.remove("current-sorting");
-        byType.classList.add("current-sorting");
-        byRating.classList.remove("current-sorting");
-      });
+  byType.addEventListener("click", function(event) {
+    event.preventDefault();
+    byPrice.classList.remove("current-sorting");
+    byType.classList.add("current-sorting");
+    byRating.classList.remove("current-sorting");
+  });
 
-      byRating.addEventListener("click", function(event) {
-        event.preventDefault();
-        byPrice.classList.remove("current-sorting");
-        byType.classList.remove("current-sorting");
-        byRating.classList.add("current-sorting");
-      });
+  byRating.addEventListener("click", function(event) {
+    event.preventDefault();
+    byPrice.classList.remove("current-sorting");
+    byType.classList.remove("current-sorting");
+    byRating.classList.add("current-sorting");
+  });
 } // закрывающая скобка иф-а отелей
 
-  if ( nameCurPage == emptyPage) {  // фильтр на пустой странице
+if (nameCurPage == emptyPage) { // фильтр на пустой странице
 
-    $( function() {
-    $( "#slider-range-empty" ).slider({
-    range: true,
-    min: 0,
-    max: 3650,
-    values: [ 0, 3000 ],
-    slide: function( event, ui ) {
-    $( "#amount" ).val( "ОТ   " + ui.values[ 0 ] + "         |         ДО " + ui.values[ 1 ] );
-    }
+  $(function() {
+    $("#slider-range-empty").slider({
+      range: true,
+      min: 0,
+      max: 3650,
+      values: [0, 3000],
+      slide: function(event, ui) {
+        $("#amount").val("ОТ   " + ui.values[0] + "         |         ДО " + ui.values[1]);
+      }
     });
-    $( "#amount" ).val( "ОТ   " + $( "#slider-range-empty" ).slider( "values", 0 ) +
-    "         |        ДО " + $( "#slider-range-empty" ).slider( "values", 1 ) );
-    } );
+    $("#amount").val("ОТ   " + $("#slider-range-empty").slider("values", 0) +
+      "         |        ДО " + $("#slider-range-empty").slider("values", 1));
+  });
 
 }
